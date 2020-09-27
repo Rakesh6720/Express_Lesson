a784bcee28ea4d536dab3c3ceb88ac0f802961ee
Rakesh Baruah <31023635+Rakesh6720@users.noreply.github.com>
2020-09-27 14:43:13 -0500

    add render function to shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    // render is Express function that uses default template engine defined in app.js
+    res.render('shop');
 });
 
 module.exports = router;
